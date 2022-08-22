1fb4fb60f6d9e60952bdd5e4e4196a9876901a92
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-08-22 01:23:56 -0500

    Add create retention method

--- src/facturapi/retentions.py
+++ src/facturapi/retentions.py
@@ -6,3 +6,15 @@ class RetentionsClient(BaseClient):
     """Retentions API client"""
 
     endpoint = "retentions"
+
+    def create(self, data: dict) -> dict:
+        """Creates a new Retention. If the receipt is created in a Live environment, it will be stamped and sent to satisfy
+
+        Args:
+            data (dict): Retention data
+
+        Returns:
+            dict: Created retention object
+        """
+        url = self._get_request_url()
+        return self._execute_request("POST", url, json_data=data).json()
