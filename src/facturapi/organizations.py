7b7463cc70c3fe467318f5e556935f43787f78bc
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-09-04 18:54:19 -0500

    Add retrieve all organizations method

--- src/facturapi/organizations.py
+++ src/facturapi/organizations.py
@@ -1,4 +1,5 @@
 """Organizations API endpoint"""
+from datetime import datetime
 from .http import BaseClient
 
 
@@ -18,3 +19,45 @@ class OrganizationsClient(BaseClient):
         """
         url = self._get_request_url()
         return self._execute_request("POST", url, json_data={"name": name}).json()
+
+    def all(
+        self,
+        search: str = None,
+        start_date: datetime = None,
+        end_date: datetime = None,
+        page: int = None,
+        limit: int = None,
+    ) -> dict:
+        """Returns a paginated list of all the organizations registered under your account
+        or makes a search according to parameters
+
+        Args:
+            search (str, optional): Text to search on commercial name, legal name or tax ID. Defaults to None.
+            start_date (datetime, optional): Lower limit of a date range. Defaults to None.
+            end_date (datetime, optional): Upper limit of a date range. Defaults to None.
+            page (int, optional): Result page to return, beginning with 1. Defaults to None.
+            limit (int, optional): Number from 1 to 50 representing the maximum quantity of
+            results to return. Used for pagination. Defaults to None.
+
+        Returns:
+            dict: List of organizations
+        """
+
+        params = {}
+        if search:
+            params.update({"q": search})
+
+        if start_date:
+            params.update({"date[gt]": start_date.astimezone().isoformat()})
+
+        if end_date:
+            params.update({"date[lt]": end_date.astimezone().isoformat()})
+
+        if page:
+            params.update({"page": page})
+
+        if limit:
+            params.update({"limit": limit})
+
+        url = self._get_request_url()
+        return self._execute_request("GET", url, params).json()
