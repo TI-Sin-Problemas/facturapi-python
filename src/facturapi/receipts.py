f9c7bcbc4fe828b99efdb6896ac6c83c36ab2d9a
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-09-04 00:01:10 -0500

    Add create receipt method

--- src/facturapi/receipts.py
+++ src/facturapi/receipts.py
@@ -6,3 +6,16 @@ class ReceiptsClient(BaseClient):
     """Receipts API client"""
 
     endpoint = "receipts"
+
+    def create(self, data: dict) -> dict:
+        """Creates a new receipt. All receipts generate a self-invoicing URL that the customer can
+        visit to fill in their tax information on a microsite with the branding of the organization
+
+        Args:
+            data (dict): Receipt data
+
+        Returns:
+            dict: Created receipt object
+        """
+        url = self._get_request_url()
+        return self._execute_request("POST", url, json_data=data).json()
