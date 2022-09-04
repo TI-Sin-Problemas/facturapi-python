f67bf001ce8dd8ace0a469086530d653e004740d
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2022-09-04 18:42:41 -0500

    Add create organization method

--- src/facturapi/organizations.py
+++ src/facturapi/organizations.py
@@ -6,3 +6,15 @@ class OrganizationsClient(BaseClient):
     """Organizations API client"""
 
     endpoint = "organizations"
+
+    def create(self, name: str) -> dict:
+        """Creates a new Organization on the user account
+
+        Args:
+            name (str): Name of the new Organization
+
+        Returns:
+            dict: Created Organization object
+        """
+        url = self._get_request_url()
+        return self._execute_request("POST", url, json_data={"name": name}).json()
