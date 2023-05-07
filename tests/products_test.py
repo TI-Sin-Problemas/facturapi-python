0bf021755ebeadaa36affe638d3c3797746d0c23
Alfredo Altamirano <Ahuahuachi@users.noreply.github.com>
2023-05-06 20:30:17 -0600

    feat(products): add get all products test

--- tests/products_test.py
+++ tests/products_test.py
@@ -16,3 +16,16 @@ class TestProducts:
 
         # Check if the result is a Product instance
         assert isinstance(self.product, Product)
+
+    def test_get_all_products(self):
+        """Test get all products"""
+
+        # Get all products
+        products = self.api.products.all()
+
+        # Check if the result is a ProductList instance
+        assert isinstance(products, ProductList)
+
+        # Check if all the items are Product instances
+        assert all(isinstance(product, Product) for product in products)
+
